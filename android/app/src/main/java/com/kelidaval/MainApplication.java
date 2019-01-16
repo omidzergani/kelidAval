0b6f5ab882b371cd009c55c0aae2662e76e454f4
omidzergani <omidzergany@gmail.com>
2019-01-17 01:43:05 +0330

    android commit

--- android/app/src/main/java/com/kelidaval/MainApplication.java
+++ android/app/src/main/java/com/kelidaval/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.airbnb.android.react.lottie.LottiePackage;
-import com.airbnb.android.react.lottie.LottiePackage;
 import com.mapbox.rctmgl.RCTMGLPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -28,8 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new LottiePackage(),
-            new LottiePackage(),
+          new LottiePackage(),
           new RCTMGLPackage(),
           new VectorIconsPackage(),
           new RNGestureHandlerPackage()
